54cfce65c20b632d1696163439e545400f3984b1
Ryan Bello <ryanbello@gmail.com>
2012-02-13 14:17:18 -0800

    test2 version, refactored but needs cleanup

--- fractal.js
+++ fractal.js
@@ -1,318 +1,273 @@
-﻿/*
- *  Complex Numbers, two components!
- */
-function Complex(real, imaginary) {
-    this.x = real;
-    this.y = imaginary;
-}
-/*
- *  Arithmetic operators for complex numbers
- */
-Complex.prototype.add = function(b) { return new Complex(this.x + b.x, this.y + b.y); };
-Complex.prototype.sub = function(b) { return new Complex(this.x - b.x, this.y - b.y); };
-Complex.prototype.mul = function(b) { return new Complex(this.x * b.x - this.y * b.y, this.x * b.y + this.y * b.x);  };
-Complex.prototype.div = function(b) { var d=(b.x*b.x+b.y*b.y); return new Complex((this.x*b.x+this.y*b.y)/d, (this.y*b.x-this.x*b.y)/d); }
-    Complex.prototype.abs = function() { return Math.sqrt(this.x*this.x + this.y*this.y); };
-Complex.prototype.neg = function() { return new Complex(-this.x, -this.y); };
-/*
- *  JavaScript type conversion operator
- */
-Complex.prototype.toString  = function() { return this.x.toString().substring(0,6) + (this.y>=0?"+":"") + this.y.toString().substring(0,6) + "·i"; };
-
-
-/*
- *  Utility functions
- */
-var busy = 0;
-function xy_to_z(x, y, width, height) {
-    var w = (width-1)/2;
-    var h = (height-1)/2;
-    x = 2 * (x - w)/w;
-    y = 2 * (h - y)/h;
-    return new Complex(x, y);
-}
-function click_to_z(event) {
-    var x = event.offsetX || (event.pageX - event.currentTarget.offsetLeft);
-    var y = event.offsetY || (event.pageY - event.currentTarget.offsetTop);
-    var z = xy_to_z(x, y, event.currentTarget.width, event.currentTarget.height);
-    return z;
-}
-function show_var_m(z0, c, n, zn) {
-    n++;
-    document.getElementById("m_z0").innerHTML = z0.toString();
-    document.getElementById("m_c").innerHTML = c.toString();
-    document.getElementById("m_n").innerHTML = n.toString();
-    document.getElementById("m_zn").innerHTML = zn.toString();
-}
-function show_var_j(z0, c, n, zn) {
-    n++;
-    document.getElementById("j_z0").innerHTML = z0.toString();
-    document.getElementById("j_c").innerHTML = c.toString();
-    document.getElementById("j_n").innerHTML = n.toString();
-    document.getElementById("j_zn").innerHTML = zn.toString();
-}
-function draw_pixel(ctx, x, y, r) {
-    var fs = r;
-    if (typeof(fs) != "string") {
-	if (r.iterations == 256) {
-            fs = "#000000";
-	} else {
-            var b = Math.floor(255-r.z.abs());
-            if (b > 255) b = 255;
-            if (b < 0) b = 0;
-            var i = Math.floor(255-16*Math.sqrt(r.iterations));
-            fs = 'rgb(' + i + ',' + i + ','+ b +')';
-	}
-    }
-    ctx.fillStyle = fs;
-    ctx.fillRect(x,y,1,1);
-}
-function grid_draw(graph, c) {
-    var canvas = graph.canvas
-        var ctx = graph.ctx;
-    ctx.strokeStyle = "rgba(200,200,200,0.9)";
-    ctx.fillStyle = "rgba(200,200,200,0.9)";
-    for (var i=0; i <= 4; i++) {
-	var x = (canvas.width-1)/4*i + 0.5;
-	ctx.beginPath();
-	ctx.moveTo(x, 0);
-	ctx.lineTo(x, canvas.height);
-	ctx.stroke();
-    }
-    for (var i=0; i <= 4; i++) {
-	var y = (canvas.width-1)/4*i + 0.5;
-	ctx.beginPath();
-	ctx.moveTo(0, y);
-	ctx.lineTo(canvas.width, y);
-	ctx.stroke();
-    }
-    if (c) {
-
-    }
-}
-
-/*
- *  Fractal essence: does z = z^2 + c diverge?
- */
-function fractal_inner_loop(z, c, max_iterations, max_magnitude) {
-    var mm2 = max_magnitude * max_magnitude;
-    var i;
-    for (i=0; i < max_iterations; i++) {
-	z = z.mul(z).add(c);
-	var m2 = z.x*z.x + z.y*z.y;
-	if (m2 > mm2) {
-            break;
-	}
-    }
-    return { "iterations": i, "z": z }
-}
-
-mandelbrot = {
-    draw: function() {
-	this.canvas = document.getElementById('mandelbrot');
-	this.ctx = this.canvas.getContext('2d');
-	this.y = 0;
-	this.draw_line();
-	busy++;
-    },
-    draw_line: function() {
-	for (var x=0; x <= this.canvas.width; x++) {
-            var z = xy_to_z(x, this.y, this.canvas.width, this.canvas.height);
-            var r = fractal_inner_loop(z, z, 256, 2);
-            draw_pixel(this.ctx, x, this.y, r);
-	}
-	if (++this.y < this.canvas.height) {
-            var _this = this;
-            setTimeout(function(){_this.draw_line();}, 0);
-	} else {
-            grid_draw(this);
-            busy--;
-	}
-    },
-    click: function(event) {
-	var z = click_to_z(event);
-	julia.draw(z);
-	var r = iterate_draw_j(z, z);
-	show_var_j(z, z, r.iterations, r.z);
-    },
-    mousemove: function(event) {
-	if (!busy) {
-            var z = click_to_z(event);
-            var r = iterate_draw_m(z, z);
-            show_var_m(z, z, r.iterations, r.z);
-	}
+﻿Function.prototype.method = function (name, func) {
+    if (!this.prototype[name]) {
+	this.prototype[name] = func;
+	return this;
     }
 };
 
-julia = {
-    c: new Complex(0,0),
-    draw: function(c) {
-	document.getElementById("point").innerHTML = c;
-	this.canvas = document.getElementById('julia');
-	this.ctx = this.canvas.getContext('2d');
-	this.y = 0;
-	this.c = c;
-	this.draw_line();
-	busy++;
-    },
-    draw_line: function() {
-	for (var x=0; x <= this.canvas.width; x++) {
-            var z = xy_to_z(x, this.y, this.canvas.width, this.canvas.height);
-            var r = fractal_inner_loop(z, this.c, 256, 2);
-            draw_pixel(this.ctx, x, this.y, r);
-	}
-	if (++this.y < this.canvas.height) {
-            var _this = this;
-            setTimeout(function(){_this.draw_line();}, 0);
-	} else {
-            grid_draw(this);
-            busy--;
-	}
-    },
-    click: function(event) {
-      var z = click_to_z(event);
-      julia.draw(z);
-      var r = iterate_draw_j(z, julia.c);
-      show_var_j(z, julia.c, r.iterations, r.z);
-    },
-    mousemove: function(event) {
-	if (!busy) {
-            var z = click_to_z(event);
-            var r = iterate_draw_j(z, julia.c);
-            show_var_j(z, julia.c, r.iterations, r.z);
-	}
+Object.method('superior', function (name) {
+	var that = this, method = that[name];
+	return function () {
+	    return method.apply(that, argumets);
+	};
+    });
+
+
+var circle = {};
+
+(function () {
+    //
+    //  Complex Numbers:  z = a + i*b
+    //  Arithmetic operators for complex numbers
+    //  JavaScript type conversion operator
+    //
+    function Complex(real, imaginary) {
+	this.a = real;
+	this.b = imaginary;
     }
-};
+    Complex.prototype.add  = function(z) { return new Complex(this.a + z.a, this.b + z.b); };
+    Complex.prototype.sub  = function(z) { return new Complex(this.a - z.a, this.b - z.b); };
+    Complex.prototype.mul  = function(z) { return new Complex(this.a * z.a - this.b * z.b, this.a * z.b + this.b * z.a);  };
+    Complex.prototype.div  = function(z) { var d=this.abs2(); return new Complex((this.a*z.a+this.b*z.b)/d, (this.b*z.a-this.a*z.b)/d); };
+    Complex.prototype.abs2 = function() { return this.a*this.a + this.b*this.b; };
+    Complex.prototype.abs  = function() { return Math.sqrt(this.abs2()); };
+    Complex.prototype.neg  = function() { return new Complex(-this.a, - this.b); };
+    Complex.prototype.toString = function() { return this.a.toString().substring(0,6) + (this.b>=0?"+":"") + this.b.toString().substring(0,6) + "&middot;i"; };
 
-function iterate_draw_m(z, c) {
-    // Poles
-    var poles = {};
-    poles.canvas = document.getElementById('mpoles');
-    poles.ctx = poles.canvas.getContext('2d');
-    poles.ctx.fillStyle = "#ffffff";
-    poles.ctx.strokeStyle = "#0000ff";
-    poles.ctx.fillRect(0, 0, poles.canvas.width, poles.canvas.height);
-    poles.mid = (poles.canvas.width-1)/2;
-
-    // Iters
-    var iters = {};
-    iters.canvas = document.getElementById('miters');
-    iters.ctx = iters.canvas.getContext('2d');
-    iters.ctx.fillStyle = "#ffffff";
-    iters.ctx.strokeStyle = "#00ff00";
-    iters.ctx.fillRect(0, 0, iters.canvas.width, iters.canvas.height);
-    iters.mid = (iters.canvas.width-1)/2;
-
-    // Fractal inner loop exposed
-    var max_magnitude = 2;
-    var max_iterations = 1000;
-    var mm2 = max_magnitude * max_magnitude;
-    poles.ctx.beginPath();
-    poles.ctx.moveTo(poles.mid+poles.mid*z.x/2, poles.mid-poles.mid*z.y/2)
-        iters.ctx.beginPath();
-    iters.ctx.moveTo(0, iters.mid-iters.mid*z.abs()/2)
-        var i;
-    for (i=0; i < max_iterations; i++) {
-	z = z.mul(z).add(c);
-
-	poles.ctx.lineTo(poles.mid+poles.mid*z.x/2, poles.mid-poles.mid*z.y/2);
-	iters.ctx.lineTo(i, iters.mid-iters.mid*z.abs()/2);
-
-	var m2 = z.x*z.x + z.y*z.y;
-	if (m2 >= mm2) {
-            break;
+    var pallet = [];
+    pallet.build = function () {
+	var max = document.getElementById('pallet').height;
+	var o = 2*Math.PI/max;
+	var p = 1/3*2*Math.PI;
+	var q = 2/3*2*Math.PI;
+	for (var i=0; i < max; i++) {
+	    var a = i*o;
+	    var r = Math.floor(128 + 128*Math.sin( a + q));
+	    var g = Math.floor(128 + 128*Math.sin( a    ));
+	    var b = Math.floor(128 + 128*Math.sin( a + p));
+	    pallet[i] = 'rgb('+r+','+g+','+b+')';
 	}
-    }
-    poles.ctx.stroke();
-    iters.ctx.stroke();
-    grid_draw(poles);
-    grid_draw(iters);
-
-    poles.ctx.strokeStyle = "#ff0000";
-    poles.ctx.beginPath();
-    poles.ctx.arc(poles.mid,poles.mid,poles.mid,0,Math.PI*2,true);
-    poles.ctx.stroke();
-
-    iters.ctx.strokeStyle = "#ff0000";
-    iters.ctx.beginPath();
-    iters.ctx.moveTo(0, 0.5);
-    iters.ctx.lineTo(iters.canvas.width, 0.5);
-    iters.ctx.moveTo(0, iters.canvas.width-0.5);
-    iters.ctx.lineTo(iters.canvas.width, iters.canvas.width-0.5);
-    iters.ctx.stroke();
-
-    return { "iterations": i, "z": z }
-}
-
-function iterate_draw_j(z, c) {
-    // Poles
-    var poles = {};
-    poles.canvas = document.getElementById('jpoles');
-    poles.ctx = poles.canvas.getContext('2d');
-    poles.ctx.fillStyle = "#ffffff";
-    poles.ctx.strokeStyle = "#0000ff";
-    poles.ctx.fillRect(0, 0, poles.canvas.width, poles.canvas.height);
-    poles.mid = (poles.canvas.width-1)/2;
-
-    // Iters
-    var iters = {};
-    iters.canvas = document.getElementById('jiters');
-    iters.ctx = iters.canvas.getContext('2d');
-    iters.ctx.fillStyle = "#ffffff";
-    iters.ctx.strokeStyle = "#00ff00";
-    iters.ctx.fillRect(0, 0, iters.canvas.width, iters.canvas.height);
-    iters.mid = (iters.canvas.width-1)/2;
-
-    // Fractal inner loop exposed
-    var max_magnitude = 2;
-    var max_iterations = 1000;
-    var mm2 = max_magnitude * max_magnitude;
-    poles.ctx.beginPath();
-    poles.ctx.moveTo(poles.mid+poles.mid*z.x/2, poles.mid-poles.mid*z.y/2)
-        iters.ctx.beginPath();
-    iters.ctx.moveTo(0, iters.mid-iters.mid*z.abs()/2)
-        var i;
-    for (i=0; i < max_iterations; i++) {
-	z = z.mul(z).add(c);
-
-	poles.ctx.lineTo(poles.mid+poles.mid*z.x/2, poles.mid-poles.mid*z.y/2);
-	iters.ctx.lineTo(i, iters.mid-iters.mid*z.abs()/2);
-
-	var m2 = z.x*z.x + z.y*z.y;
-	if (m2 >= mm2) {
-            break;
+    };
+    pallet.show = function () {
+	var max = document.getElementById('pallet').height;
+	var ctx = document.getElementById('pallet').getContext('2d');
+	for (var i=0; i < max; i++) {
+	    ctx.fillStyle = pallet[i];
+	    ctx.fillRect(0,i,ctx.canvas.width,1);
 	}
-    }
-    poles.ctx.stroke();
-    iters.ctx.stroke();
-    grid_draw(poles);
-    grid_draw(iters);
-
-    poles.ctx.strokeStyle = "#ff0000";
-    poles.ctx.beginPath();
-    poles.ctx.arc(poles.mid,poles.mid,poles.mid,0,Math.PI*2,true);
-    poles.ctx.stroke();
-
-    iters.ctx.strokeStyle = "#ff0000";
-    iters.ctx.beginPath();
-    iters.ctx.moveTo(0, 0.5);
-    iters.ctx.lineTo(iters.canvas.width, 0.5);
-    iters.ctx.moveTo(0, iters.canvas.width-0.5);
-    iters.ctx.lineTo(iters.canvas.width, iters.canvas.width-0.5);
-    iters.ctx.stroke();
-
-    return { "iterations": i, "z": z }
-}
+    };
 
 
-function onload() {
-    //var z = new Complex(-0.5, 0.55);
-    var z = new Complex(-0.092, 0.996);
-    mandelbrot.draw();
-    julia.draw(z);
-    iterate_draw_m(z, z);
-    iterate_draw_j(z, z);
-    show_var_m(z, z, 1000, 1/0);
-    show_var_j(z, z, 1000, 1/0);
+    var draw_pixel = function (ctx, x, y, fs) {
+	ctx.fillStyle = fs;
+	ctx.fillRect(x,y,1,1);
+    };
+    var mouse_to_xy = function (event) {
+	var x = event.offsetX || (event.pageX - event.currentTarget.offsetLeft);
+	var y = event.offsetY || (event.pageY - event.currentTarget.offsetTop);
+	return {x:x, y:y};
+    };
 
-}
+    var fractal = function (spec, my) {
+	spec = spec || {};
+	spec.itrMax  = spec.itrMax  || 1024;
+	spec.pallet  = spec.pallet  || pallet;
+	spec.bound   = spec.bound   || 2.0;
+	spec.id      = spec.id      || "fractal";
+	spec.idPts   = spec.idPts   || spec.id+"Pts";
+	spec.idItr   = spec.idItr   || spec.id+"Itr";
+	spec.set     = spec.set     || document.getElementById(spec.id).getContext('2d');
+	spec.pts     = spec.pts     || document.getElementById(spec.idPts).getContext('2d');
+	spec.itr     = spec.itr     || document.getElementById(spec.idItr).getContext('2d');
+	spec.tl      = spec.tl      || new Complex(-2,+2);
+	spec.br      = spec.br      || new Complex(+2,-2);
+	spec.func    = spec.func    || function(z) { return z; };
+
+	spec.set.canvas.setAttribute('onclick', spec.id + '.click(event)');
+	spec.set.canvas.setAttribute('onmousemove', spec.id + '.mousemove(event)');
+
+	var that = {};
+	my = my || {}
+
+	var xy_to_z = function (x, y) {
+	    x = x / (spec.set.canvas.width-1) * (spec.br.a - spec.tl.a) + spec.tl.a;
+	    y = y / (spec.set.canvas.height-1) * (spec.br.b - spec.tl.b) + spec.tl.b;
+	    return new Complex(x, y);
+	};
+	var z_to_xy = function (z) {
+	    var x = (z.a - spec.tl.a) / (spec.br.a - spec.tl.a) * (spec.set.canvas.width-1);
+	    var y = (z.b - spec.tl.b) / (spec.br.b - spec.tl.b) * (spec.set.canvas.height-1);
+	    return {x:x, y:y};
+	};
+
+	my.iterate = function (z, c, inspect) {
+	    var bound2 = spec.bound * spec.bound;
+	    var zPrev;
+	    for (var i=0; i < spec.itrMax; i++) {
+		if (inspect) { inspect(i, z, c); }
+		if (z.abs2() > bound2) {
+		    break;
+		}
+		zPrev = z;
+		z = spec.func(z, c);
+	    }
+	    return {i:i, z:z, zPrev:zPrev};
+	};
+	my.inspect_setup = function(z, c) {
+	    spec.pts.fillStyle = "#ffffff";
+	    spec.pts.fillRect(0, 0, spec.pts.canvas.width, spec.pts.canvas.height);
+
+	    spec.pts.fillStyle = "#0000ff";
+	    var x = (z.a+2)/4*(spec.pts.canvas.width-1);
+	    var y = (z.b-2)/-4*(spec.pts.canvas.height-1);
+	    spec.pts.fillRect(x, y, 1, 1);
+
+	    spec.pts.strokeStyle = "#0000ff";
+	    spec.pts.beginPath();
+	    spec.pts.moveTo(x, y);
+
+	    spec.itr.fillStyle = "#ffffff";
+	    spec.itr.strokeStyle = "#00ff00";
+	    spec.itr.fillRect(0, 0, spec.itr.canvas.width, spec.itr.canvas.height);
+	    spec.itr.beginPath();
+	    var mag = (spec.itr.canvas.height-1) - (z.abs())/spec.bound*(spec.itr.canvas.height-1);
+	    spec.itr.moveTo(0, mag);
+	};
+	my.inspect_step = function(i, z, c) {
+	    var x = (z.a - -2) / (+2 - -2) * (spec.pts.canvas.width-1);
+	    var y = (z.b - +2) / (-2 - +2) * (spec.pts.canvas.height-1);
+	    spec.pts.lineTo(x, y);
+
+	    var mag = (spec.itr.canvas.height-1) - (z.abs())/spec.bound*(spec.itr.canvas.height-1);
+	    spec.itr.lineTo(i, mag);
+	};
+	my.inspect_stroke = function() {
+	    spec.pts.stroke();
+	    spec.itr.stroke();
+
+	    spec.pts.strokeStyle = "#ff0000";
+	    spec.pts.beginPath();
+	    var x = (spec.pts.canvas.width-1)/2;
+	    var y = (spec.pts.canvas.height-1)/2;
+	    spec.pts.arc(x,y,x,0,Math.PI*2,true);
+	    spec.pts.stroke();
+	};
+	my.colorize = function(z, c, i, zPrev) {
+	    if (i == spec.itrMax) {
+		return '#000000';
+	    }
+	    var deband=0;
+	    if (zPrev) {
+		var scale = new Complex(spec.bound/zPrev.abs(), 0);
+		var max = zPrev.mul(scale);
+		max = spec.func(max, c);
+		var minpower = max.abs();
+		var maxpower = spec.bound;
+		deband = (minpower - z.abs())/(minpower - maxpower);
+	    }
+	    //return pallet[Math.floor(10*(i + deband)) % pallet.length];
+	    return pallet[i*13 % pallet.length];
+	};
+	my.show_vars = function (z0, c, n, zn) {
+	    var set = function(name, value) {
+		(document.getElementById(spec.id+"_"+name) || {}).innerHTML = (value || "").toString(); 
+	    };
+	    set("z0", z0);
+	    set("c", c);
+	    set("n", n);
+	    set("zn", zn);
+	};
+	my.grid = function () {
+	    var a, b, from, to;
+
+	    spec.set.strokeStyle = "rgba(200,200,200,0.9)";
+	    for (a=-2; a <= 2; a += 1) {
+		from = z_to_xy(new Complex(a, +2));
+		to = z_to_xy(new Complex(a, -2));
+
+		spec.set.beginPath();
+		spec.set.moveTo(from.x, from.y);
+		spec.set.lineTo(to.x, to.y);
+		spec.set.stroke();
+	    }
+	    for (b=-2; b <= 2; b += 1) {
+		from = z_to_xy(new Complex(-2, b));
+		to = z_to_xy(new Complex(+2, b));
+
+		spec.set.beginPath();
+		spec.set.moveTo(from.x, from.y);
+		spec.set.lineTo(to.x, to.y);
+		spec.set.stroke();
+	    }
+	};
+
+	that.set_c = function(z) {
+	    return new Complex(0, 0);
+	};
+	that.draw = function() {
+	    my.line = 0;
+	    my.draw_line();
+	};
+	my.draw_line = function() {
+	    for (var x=0; x <= spec.set.canvas.width; x++) {
+		var z = xy_to_z(x, my.line);
+		var c = that.set_c(z);
+		var r = my.iterate(z, c);
+		var color = my.colorize(r.z, c, r.i, r.zPrev);
+		draw_pixel(spec.set, x, my.line, color);
+	    }
+	    if (++my.line <= spec.set.canvas.height-1) {
+		setTimeout(my.draw_line, 0);
+	    } else {
+		my.grid();
+	    }
+	};
+	that.click = function(event) {
+	    var xy = mouse_to_xy(event);
+	    var z = xy_to_z(xy.x, xy.y);
+	    var dx = spec.br.a - spec.tl.a;
+	    var dy = spec.tl.b - spec.br.b;
+
+	    dx = dx/2;
+	    dy = dy/2;
+
+	    spec.tl = new Complex(z.a - dx/2, z.b + dy/2);
+	    spec.br = new Complex(z.a + dx/2, z.b - dy/2);
+
+	    that.draw();
+	};
+	that.mousemove = function(event) {
+	    var xy = mouse_to_xy(event);
+	    var z = xy_to_z(xy.x, xy.y);
+	    var c = that.set_c(z);
+	    my.inspect_setup(z, c);
+	    var r = my.iterate(z, c, my.inspect_step);
+	    my.inspect_stroke();
+	    my.show_vars(z, c, r.i, r.z);
+	};
+
+	that.get_name = function() {return spec.id;};
+	that.says = function() {return spec.saying || '';};
+
+	return that;
+    };
+
+    var mandlebrot = function(spec, my) {
+	spec.func  = spec.func || function(z, c) { return z.mul(z).add(c); };
+	var that = fractal(spec);
+	that.set_c = function(z) { return z; };
+	return that;
+    };
+
+    load = function () {
+	pallet.build();
+	pallet.show();
+
+	circle = fractal({id:'circle', itrMax:3});
+	// circle.draw();
+
+	mb = mandlebrot({id:'mb', itrMax:2048});
+	mb.draw();
+
+    };
+})();
